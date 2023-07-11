c6adb920098c47368afde505df2ec905d464c59a
jesusManuelJuarez <chuy.america.az@gmail.com>
2023-07-11 03:51:44 -0600

    add swagger /api-docs

--- dist/main.js
+++ dist/main.js
@@ -8,13 +8,17 @@ const DataRouter_1 = require("./data/infrastructure/DataRouter");
 const FiscalRouter_1 = require("./fiscal/infrastructure/FiscalRouter");
 const AddressRouter_1 = require("./address/infrastructure/AddressRouter");
 const signale_1 = require("signale");
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const yamljs_1 = __importDefault(require("yamljs"));
 const app = (0, express_1.default)();
 const signale = new signale_1.Signale();
 app.use(express_1.default.json());
+const swaggerDocument = yamljs_1.default.load('swagger.yaml');
 // Rutas para los recursos de data fiscal, source y address
 app.use("/data", DataRouter_1.dataRouter);
 app.use("/fiscal", FiscalRouter_1.fiscalRouter);
 app.use("/address", AddressRouter_1.addressRouter);
+app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocument));
 app.listen(3000, () => {
     signale.success("[Application] Server online on port 3000");
 });
--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,26 @@ import { dataRouter } from "./data/infrastructure/DataRouter";
 import { fiscalRouter } from "./fiscal/infrastructure/FiscalRouter";
 import { addressRouter } from "./address/infrastructure/AddressRouter";
 import { Signale } from "signale";
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 
 const app = express();
+
+
+
 const signale = new Signale();
 
 app.use(express.json());
+const swaggerDocument = YAML.load('swagger.yaml');
+
 
 // Rutas para los recursos de data fiscal, source y address
 app.use("/data", dataRouter);
 app.use("/fiscal", fiscalRouter);
 app.use("/address", addressRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.listen(3000, () => {
   signale.success("[Application] Server online on port 3000");
